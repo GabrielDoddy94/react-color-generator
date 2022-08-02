51e2216dfd2d24f382d303ffaf72f4de2e8a2e0e
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-08-02 18:36:41 -0300

    launched project

---
+++ src/SingleColor.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+type SingleColorProps = {
+  rgb: number[];
+  weight: number;
+  index: number;
+  hexColor: string;
+};
+
+export function SingleColor({
+  rgb,
+  weight,
+  index,
+  hexColor,
+}: SingleColorProps) {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(",");
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 && "color-light"}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">{hexValue}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  );
+}
